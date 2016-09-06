8c29333498b1fca1e6be7503df155b1362b9f9de
awillemant <amaury.willemant@atos.net>
2016-09-06 18:49:51 +0200

    ajout d'une assertions pour la couverture

---
+++ puho-webservice/src/test/java/puho/ApplicationTests.java
@@ -0,0 +1,14 @@
+package puho;
+
+import org.junit.Test;
+
+public class ApplicationTests {
+
+    @Test
+    public void shouldExecuteMain() throws Exception {
+        //GIVEN
+        //WHEN
+        Application.main(new String[]{});
+        //THEN
+    }
+}
